b84e76456dc46be86868814521228bd34819a5db
Ernst Becker <81073546+ErnstBecker@users.noreply.github.com>
2026-06-15 20:37:49 -0300

    refactor(commands): introduce CommandManager builder pattern

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,9 @@ mod models;
 mod services;
 mod utils;
 
-use std::sync::Arc;
 use serenity::prelude::*;
 use handlers::EventHandler;
 use commands::CommandManager;
-use commands::ping::PingCommand;
-use commands::wallet::WalletCommand;
-use client::klever::KleverClient;
-use services::wallet::WalletService;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -24,16 +19,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let intents = GatewayIntents::GUILD_MESSAGES
         | GatewayIntents::MESSAGE_CONTENT;
 
-    let wallet_service = Arc::new(WalletService::new(Arc::new(KleverClient::default())));
-
-    let commands = vec![
-        Box::new(PingCommand) as Box<dyn commands::Command>,
-        Box::new(WalletCommand { service: wallet_service }) as Box<dyn commands::Command>,
-    ];
+    let commands = CommandManager::build();
 
     let mut client = Client::builder(&token, intents)
         .event_handler(EventHandler {
-            command_manager: CommandManager::new(commands),
+            command_manager: commands,
         })
         .await?;
 
